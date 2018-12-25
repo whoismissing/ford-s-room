46eee799d06d753824ca65a559232eecc193a447
whoismissing <whereismissing@gmail.com>
2018-12-25 14:47:43 -0600

    Updated mediawiki install script, tested on Ubuntu 18

--- mediawiki/install_mediawiki131.sh
+++ mediawiki/install_mediawiki131.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # https://www.mediawiki.org/wiki/Manual:Running_MediaWiki_on_Debian_or_Ubuntu
-# Tested on Ubuntu Server 17.10
+# Tested on Ubuntu Server 17.10 and Ubuntu Server 18.04.1 LTS with some adjustments
+# To install php7.1-mbstring, enable universe in /etc/apt/sources.list
+# deb http://us.archive.ubuntu.com/ubuntu/ artful universe main
+# deb http://us.archive.ubuntu.com/ubuntu/ artful-security universe main
 # Mediawiki v1.31
 # Script format modified from install_drupal730.sh
 
@@ -27,18 +30,16 @@ fi
 # Modify vars as seen fit
 rootdbpasswd="P@ssw0rd"
 usrdbpasswd="P@ssw0rd"
-ipaddr=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+ipaddr=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}') # or use hostname -I
 url="localhost"
-
-echo "==================================================="
-echo "Database settings will be:"
-echo -e "mysql users\nroot:$rootdbpasswd\nwikidbadmin:$usrdbpasswd\n"
-echo "database name = wikidb"
 
 echo -e "\n[+] Installing LAMP + dependencies\n"
 echo -e "cmd:\tapt-get install apache2 mysql-server php7.1 php7.1-mysql libapache2-mod-php7.1 php7.1-gd php7.1-xml php7.1-mbstring\n"
 # Download LAMP packages and dependencies [ user will be prompted to set mysql-server password ]
 apt-get install apache2 mysql-server php7.1 php7.1-mysql libapache2-mod-php7.1 php7.1-gd php7.1-xml php7.1-mbstring
+# Enable universe repository for php7.2-mbstring when running on Ubuntu 18.04.1
+# add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+# apt-get install apache2 mysql-server php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-gd php7.2-xml php7.2-cmmon php7.2-mbstring
 
 echo -e "\n[+] Getting mediawiki tar and copying to /var/www/html/\n"
 # Get mediawiki v1.31
@@ -46,6 +47,21 @@ wget https://releases.wikimedia.org/mediawiki/1.31/mediawiki-1.31.1.tar.gz
 tar -xzf mediawiki-1.31.1.tar.gz
 cp -r mediawiki-1.31.1/* /var/www/html/
 mv /var/www/html/index.html /var/www/
+
+# On Ubuntu 18, mysql-server package will have no mysql server password prompt, instead need to set password via sudo mysql_secure_installation, so pause here
+echo "==================================================="
+echo "Database settings will be:"
+echo -e "mysql users\nroot:$rootdbpasswd\nwikidbadmin:$usrdbpasswd\n"
+echo "database name = wikidb"
+echo "If script is being run on Ubuntu 18, pause here, otherwise just skip this"
+echo "In another terminal, run sudo mysql_secure_installation, press Y when finished"
+read -n1 -r -p "> " key
+if [ "$key" = 'Y' ]; then
+    echo -e "\nContinuing..."
+else
+    echo -e "\nExiting..."
+    exit 99
+fi
 
 echo -e "\n[+] Setting up initial mysql db\n"
 # Set up mysql database for mediawiki
